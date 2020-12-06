d59fb52804ba237c24bdfb9225b7928303737444
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-06 12:34:55 +0530

    [Arti Dandge] Add : Ability to add Person Contact details from console

---
+++ AddressBookSystemUsingCSharp/AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystemUsingCSharp
+{
+    class AddressBook
+    {
+        List<Contacts> contactList;
+        public AddressBook()
+        {
+            this.contactList = new List<Contacts>();
+        }
+        public void AddContact(string first_name, string LastName, string address, string city, string state, int zip, long phone_number, string email)
+        {
+            Contacts contact = new Contacts(first_name, LastName, address, city, state, zip, phone_number, email);
+            contactList.Add(contact);
+            Console.WriteLine("Contact added Successfully !");
+        }   
+    }
+}
