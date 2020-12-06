191e631fb82fe7926aac134460ce89f429067535
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-06 12:06:28 +0530

    [Arti Dandge] Add : Ability to create contact in Address Book

--- AddressBookSystemUsingCSharp/AddressBookMain.cs
+++ AddressBookSystemUsingCSharp/AddressBookMain.cs
@@ -7,6 +7,7 @@ namespace AddressBookSystemUsingCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System !");
+          
         }
     }
 }
---
+++ AddressBookSystemUsingCSharp/Contacts.cs
@@ -0,0 +1,34 @@
+﻿
+namespace AddressBookSystemUsingCSharp
+{
+    public class Contacts
+    {
+        public string first_name;
+        public string last_name;
+        public string address;
+        public string city;
+        public string state;
+        public int zip;
+        public long phone_number;
+        public string email;
+
+        public Contacts(string first_name, string last_name, string address, string city, string state, int zip, long phone_number, string email)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone_number = phone_number;
+            this.email = email;
+        }
+        public string tostring()
+        {
+
+            return "Person Details: \tFirst name:" + this.first_name + " \tLast name:" + this.last_name + " \t Address:" + this.address + " \tCity:" + this.city + " \tState:"
+                    + this.state + " \tZip:" + this.zip + " \tPhone Number:" + this.phone_number + " \tEmail:" + this.email;
+        }
+    }
+}
+
