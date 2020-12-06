70b1ba779c84b18d3aa5f07c7cb613480bfcb712
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-06 13:05:25 +0530

    [Arti Dandge] Add : Ability to edit existing person's details from his first name

--- AddressBookSystemUsingCSharp/AddressBook.cs
+++ AddressBookSystemUsingCSharp/AddressBook.cs
@@ -16,6 +16,34 @@ namespace AddressBookSystemUsingCSharp
             Contacts contact = new Contacts(first_name, LastName, address, city, state, zip, phone_number, email);
             contactList.Add(contact);
             Console.WriteLine("Contact added Successfully !");
-        }   
+        }
+        public void EditContact(string first_name)
+        {           
+            foreach (Contacts contact in contactList)
+            {
+                if (first_name.Equals(contact.first_name))
+                {
+                    Console.WriteLine("Enter Last Name : ");
+                    contact.last_name = Console.ReadLine();
+                    Console.WriteLine("Enter Address: ");
+                    contact.address = Console.ReadLine();
+                    Console.WriteLine("Enter City : ");
+                    contact.city = Console.ReadLine();
+                    Console.WriteLine("Enter State : ");
+                    contact.state = Console.ReadLine();
+                    Console.WriteLine("Enter Zip code : ");
+                    contact.zip = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Phone Number : ");
+                    contact.phone_number = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter Email : ");
+                    contact.email = Console.ReadLine();
+                    Console.WriteLine("Contact Updated Successfully !");
+                }
+                else
+                {
+                    Console.WriteLine("Contact not found with first name '{0}'!", first_name);
+                }
+            }
+        }
     }
 }
--- AddressBookSystemUsingCSharp/AddressBookMain.cs
+++ AddressBookSystemUsingCSharp/AddressBookMain.cs
@@ -8,44 +8,54 @@ namespace AddressBookSystemUsingCSharp
         {
             Console.WriteLine("Welcome to Address Book System !");
             AddressBook addressBook = new AddressBook();
-            Console.WriteLine("Choose option to procced further \n1.Add Contact \n2.Exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            string isRepeat = "yes";
+            do
             {
-                case 1:
-                    try
-                    {
-                        Console.WriteLine("Enter First Name : ");
-                        string firstName = Console.ReadLine();
-                        Console.WriteLine("Enter Last Name : ");
-                        string lastName = Console.ReadLine();
-                        Console.WriteLine("Enter Address : ");
-                        string address = Console.ReadLine();
-                        Console.WriteLine("Enter City : ");
-                        string city = Console.ReadLine();
-                        Console.WriteLine("Enter State : ");
-                        string state = Console.ReadLine();
-                        Console.WriteLine("Enter Zip : ");
-                        int zip = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter Phone Number : ");
-                        long phoneNumber = Convert.ToInt64(Console.ReadLine());
-                        Console.WriteLine("Enter Email id :");
-                        string email = Console.ReadLine();
-                        addressBook.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Zip and Phone number must be integers only");
-                    }
+            Console.WriteLine("Choose option to procced further \n1.Add Contact \n2.Edit Contact \n3.Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+                switch (choice)
+                {
+                    case 1:
+                        try
+                        {
+                            Console.WriteLine("Enter First Name : ");
+                            string firstName = Console.ReadLine();
+                            Console.WriteLine("Enter Last Name : ");
+                            string lastName = Console.ReadLine();
+                            Console.WriteLine("Enter Address : ");
+                            string address = Console.ReadLine();
+                            Console.WriteLine("Enter City : ");
+                            string city = Console.ReadLine();
+                            Console.WriteLine("Enter State : ");
+                            string state = Console.ReadLine();
+                            Console.WriteLine("Enter Zip : ");
+                            int zip = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Phone Number : ");
+                            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                            Console.WriteLine("Enter Email id :");
+                            string email = Console.ReadLine();
+                            addressBook.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Zip and Phone number must be integers only");
+                        }
 
-                    break;
-                case 2:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Please inter valid integer option only ");
-                    break;
-            }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter first name of person whoes details you want to edit ");
+                        string editPersonDetails = Console.ReadLine();
+                        addressBook.EditContact(editPersonDetails);
+                        break;
+                    case 3:
+                        isRepeat = "no";
+                        break;
+                    default:
+                        Console.WriteLine("Please inter valid integer option only ");
+                        break;
+                }
+            } while (isRepeat != "no");
         }
        
     }
